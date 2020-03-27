e1c6adeaae380988e416a5b4a05caa8b80b8a884
Salih Babekr <salihmorta17@gmail.com>
2020-03-27 12:53:21 -0400

    Completed login functionality, and rendered data from get api

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute';
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute exact path="/protected" component={BubblePage} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
